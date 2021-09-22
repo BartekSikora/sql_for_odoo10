6fa49e29f715cb41774ac931c8e38d48c75cf0fa
Bartek Sikora <58654131+BartekSikora@users.noreply.github.com>
2021-09-22 22:26:24 +0200

    Update product_stock_with_location.sql

--- product_stock_with_location.sql
+++ product_stock_with_location.sql
@@ -1,5 +1,5 @@
--- Po eksporcie zostanie pobrany plik CSV zawierający listę produktów z informacją na jakiej strefie i w jakiej ilości się tam znajdują . 
--- W Excelu może występować problem z formatowaniami np. liczb czy polskich znaków. W takim przypadku plik należy otworzyć przy użyciu darmowego OpenOffice Calc z zestawem znaków UTF-8. 
+-- Wykonanie zapytania pobierze plik CSV z listą produktów z informacjami gdzie się znajdują oraz w jakich ilościach.
+-- Do otwarcia pliku sugerujemy OpenOffice Calc z zestawem znaków UTF-8. W Excel przy domyślnych ustawieniach mogą wystąpić problemy z formatowaniem liczb i polskich znaków.
 
 SELECT 
 product_template.name AS "Nazwa produktu", 
@@ -15,9 +15,12 @@ product_product.default_code AS "default_code",
 product_product.barcode AS "barcode", 
 stock_location.name AS "location_name", 
 stock_quant.qty AS "quantity" 
-FROM stock_quant 
+FROM stock_quant
 LEFT JOIN product_product ON product_product.id = stock_quant.product_id 
-LEFT JOIN stock_location ON stock_location.id = stock_quant.location_id) AS product_data 
-LEFT JOIN product_template ON product_data.id = product_template.id 
-WHERE product_data.location_name NOT SIMILAR TO '%(Customers|Vendors|adjustment|Przyjęcia|Input)%' 
+LEFT JOIN stock_location ON stock_location.id = stock_quant.location_id) AS product_data
+
+LEFT JOIN product_template ON product_data.id = product_template.id
+
+WHERE product_data.location_name NOT SIMILAR TO '%(Customers|Vendors|adjustment|Przyjęcia|Input)%'
+
 ORDER BY product_template.name ASC
