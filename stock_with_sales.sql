d8e07d683f2ac1f1ff9d93e0641a5cb8b477928c
Bartek Sikora <58654131+BartekSikora@users.noreply.github.com>
2021-10-15 19:31:49 +0200

    Update and rename (beta)_stock_with_sales.sql to stock_with_sales.sql
    Added filtering on invoice line date and type, added column with warning, changed file name, fixed inconsistency

--- stock_with_sales.sql
+++ stock_with_sales.sql
@@ -8,7 +8,11 @@ product_stock.qty AS "Stan magazynowy",
 
 CASE WHEN ROUND(SUM(account_invoice_line.quantity)) > 0 THEN ROUND(SUM(account_invoice_line.quantity))
 ELSE '0'
-END AS "Sprzedana ilość" 
+END AS "Sprzedana ilość",
+
+CASE WHEN  product_stock.qty >= ROUND(SUM(account_invoice_line.quantity)) THEN 'Wystarczający stan'
+ELSE 'Uwaga - niewystarczający stan'
+END AS "Ostrzeżenie" 
 
 FROM 
 ( 
@@ -16,7 +20,7 @@ SELECT
 product_product.id AS product_id, 
 product_product.default_code AS default_code, 
 product_product.product_tmpl_id AS template_id,
-SUM(qty) AS qty
+SUM(stock_quant.qty) AS qty
 FROM stock_quant
 
 LEFT JOIN product_product ON product_product.id = stock_quant.product_id 
@@ -27,6 +31,10 @@ GROUP BY product_product.id, product_product.default_code) AS product_stock
 
 LEFT JOIN account_invoice_line ON account_invoice_line.product_id = product_stock.product_id 
 LEFT JOIN product_template ON product_template.id = product_stock.template_id
+
+--- W wierszu poniżej należy zdefiniować zakres dat faktur, z których ma być pobierana sprzedaż
+WHERE account_invoice_line.write_date BETWEEN '2021-10-01' AND '2021-10-31'
+AND account_invoice_line.line_type = 'out_invoice'
 
 GROUP BY product_stock.default_code, product_template.name, product_stock.qty
 
